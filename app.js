7b5d3f9aebf69afee8bf007d4e85d2291088f6b5
fercholo0730 <fercholo0730@hotmail.com>
2021-11-05 16:16:57 -0500

    final commit

--- app.js
+++ app.js
@@ -33,10 +33,11 @@ class Products {
       let data = await result.json();
       let products = data.items;
       products = products.map((item) => {
-        const { title, price } = item.fields;
+        const { title, price, description } = item.fields;
         const { id } = item.sys;
         const image = item.fields.image.fields.file.url;
-        return { title, price, id, image };
+        const icon = item.fields.icon.fields.file.url;
+        return { title, price, description, id, image, icon };
       });
       return products;
     } catch (error) {
@@ -57,7 +58,7 @@ class UI {
                 <div class="img-container">
                     <div class="bckground-product col">
                         <div class="d-flex justify-content-center">
-                            <img class="flaticon-products pt-5 pb-3" src="img/hairdryer.png" alt="">
+                            <img class="flaticon-products pt-5 pb-3" src=${product.icon} alt="">
                         </div>
                         <div class="d-flex justify-content-center">
                             <div class="horiz-line"></div>
@@ -66,7 +67,7 @@ class UI {
                             <h3 class="product-title pt-3">${product.title}</h3>
                         </div>
                         <div class="d-flex justify-content-center">
-                            <h6 class="product-description text-center pt-1">Servicio completo sssss de peluquería</h6>
+                            <h6 class="product-description text-center pt-1">${product.description}</h6>
                         </div>
                         <img src=${product.image} alt="" class="product-img">
                         <button class="bag-btn" data-id=${product.id}>
@@ -76,7 +77,7 @@ class UI {
                     </div>
                 </div>
                 <div>
-                    <h4 class="product-price pt-1">COP $ 100.000</h4>
+                    <h4 class="product-price pt-1">COP $ ${product.price}</h4>
                 </div>
             </article>
             <!--------------- end of single product --------------->
@@ -108,10 +109,24 @@ class UI {
         //Set cart values
         this.setCartValues(cart);
         //Display cart itmes
+        this.addCartItem(cartItems);
         //Show the cart
+        this.showCart();
       });
     });
   }
+  // setCartValues(cart) {
+  //   let tempTotal = 0;
+  //   let itemsTotal = 0;
+  //   cart.map((item) => {
+  //     tempTotal += item.price * item.amount;
+  //     itemsTotal += item.amount;
+  //   });
+  //   cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+  //   cartItems.innerText = itemsTotal;
+  //   console.log(cartTotal);
+  //   console.log(cartItems);
+  // }
   setCartValues(cart) {
     let tempTotal = 0;
     let itemsTotal = 0;
@@ -121,15 +136,102 @@ class UI {
     });
     cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
     cartItems.innerText = parseInt(itemsTotal);
-    console.log(cartTotal);
-    console.log(cartItems);
+  }
+  addCartItem(item) {
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+    div.innerHTML = `
+    <img src=${item.image} alt="" srcset="">
+                    <div>
+                        <h4>${item.title}</h4>
+                        <h5>$${item.price}</h5>
+                        <span class="remove-item" data-id=${item.id}>Remover</span>
+                    </div>
+                    <div>
+                        <i class="fa fa-chevron-up" data-id=${item.id}></i>
+                        <p class="item-amount">${item.amount}</p>
+                        <i class="fa fa-chevron-down" data-id=${item.id}></i>
+                    </div>
+    `;
+    cartContent.appendChild(div);
+  }
+  showCart() {
+    cartOverlay.classList.add("transparentBcg");
+    cartDom.classList.add("showCart");
+  }
+  setupAPP() {
+    cart = Storage.getCart();
+    this.setCartValues(cart);
+    this.populateCart(cart);
+    cartBtn.addEventListener("click", this.showCart);
+    closeCartBtn.addEventListener("click", this.hideCart);
+  }
+  populateCart(cart) {
+    cart.forEach((item) => this.addCartItem(item));
+  }
+  hideCart() {
+    cartOverlay.classList.remove("transparentBcg");
+    cartDom.classList.remove("showCart");
+  }
+  cartLogic() {
+    clearCartBtn.addEventListener("click", () => {
+      this.clearCart();
+    });
+    //Cart functionality
+    cartContent.addEventListener("click", (event) => {
+      if (event.target.classList.contains("remove-item")) {
+        let removeItem = event.target;
+        let id = removeItem.dataset.id;
+        cartContent.removeChild(removeItem.parentElement.parentElement);
+        this.removeItem(id);
+      } else if (event.target.classList.contains("fa-chevron-up")) {
+        let addAmount = event.target;
+        let id = addAmount.dataset.id;
+        let tempItem = cart.find((item) => item.id === id);
+        tempItem.amount = tempItem.amount + 1;
+        Storage.saveCart(cart);
+        this.setCartValues(cart);
+        addAmount.nextElementSibling.innerText = tempItem.amount;
+      } else if (event.target.classList.contains("fa-chevron-down")) {
+        let lowerAmount = event.target;
+        let id = lowerAmount.dataset.id;
+        let tempItem = cart.find((item) => item.id === id);
+        tempItem.amount = tempItem.amount - 1;
+        if (tempItem.amount > 0) {
+          Storage.saveCart(cart);
+          this.setCartValues(cart);
+          lowerAmount.previousElementSibling.innerText = tempItem.amount;
+        } else {
+          cartContent.removeChild(lowerAmount.parentElement.parentElement);
+          this.removeItem(id);
+        }
+      }
+    });
+  }
+  clearCart() {
+    let cartItems = cart.map((item) => item.id);
+    cartItems.forEach((id) => this.removeItem(id));
+    console.log(cartContent.children);
+
+    while (cartContent.children.length > 0) {
+      cartContent.removeChild(cartContent.children[0]);
+    }
+    this.hideCart();
+  }
+  removeItem(id) {
+    cart = cart.filter((item) => item.id !== id);
+    this.setCartValues(cart);
+    Storage.saveCart(cart);
+    let button = this.getSingleButton(id);
+    button.disabled = false;
+    button.innerHTML = `<i class="fas fa-shopping-cart"></i>Agregar al carrito`;
+  }
+  getSingleButton(id) {
+    return buttonsDOM.find((button) => button.dataset.id === id);
   }
 }
-//End of Display products
 
 //Local storage
-
-//End of Local storage
 class Storage {
   static saveProducts(products) {
     localStorage.setItem("products", JSON.stringify(products));
@@ -141,12 +243,17 @@ class Storage {
   static saveCart(cart) {
     localStorage.setItem("cart", JSON.stringify(cart));
   }
+  static getCart() {
+    return localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+  }
 }
 //
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
-
+  ui.setupAPP();
   //get all products
   // products.getProducts().then((data) => console.log(products));
   products
@@ -157,5 +264,6 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .then(() => {
       ui.getBagButtons();
+      ui.cartLogic();
     });
 });
